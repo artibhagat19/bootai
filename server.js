f46815e7fec4ebe253583d4134536729ae3c4003
artibhagat19 <artibhagat23@navgurukul.org>
2025-06-16 09:11:19 +0530

    I added new code

--- server.js
+++ server.js
@@ -4,10 +4,21 @@
 
 const http = require('http');
 const app = require('./main');
+const express = require("express");
+const path = require("path");
+
+
 
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
 
 server.listen(PORT, () => {
   console.log(`✅ Server running at http://localhost:${PORT}`);
